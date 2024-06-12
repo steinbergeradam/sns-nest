3edae02f512db296348d589204390a83e6f6f7e5
Adam Steinberger <adam@asteinbe.com>
2024-06-12 11:52:01 -0400

    remove profile module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,14 @@ import { DatabaseModule } from './database/database.module';
 import { FriendModule } from './friend/friend.module';
 import { PostModule } from './post/post.module';
 import { CommentModule } from './comment/comment.module';
-import { ProfileModule } from './profile/profile.module';
 
 @Module({
-  imports: [DatabaseModule, AuthModule, FriendModule, PostModule, CommentModule, ProfileModule],
+  imports: [
+    DatabaseModule,
+    AuthModule,
+    FriendModule,
+    PostModule,
+    CommentModule,
+  ],
 })
 export class AppModule {}
